e41d9d308551849b2204fb01c9d334822e19a071
Hagar Hassan <hagar.hassan@nweave.com>
2024-08-03 17:09:18 +0300

    updates into color header of task

--- app/data/SchaduleData.tsx
+++ app/data/SchaduleData.tsx
@@ -4,11 +4,12 @@ export let timelineResourceData: Object[] = [
     {
         Id: 1,
         Subject: 'Work site',
-        StartTime: new Date(2024, 7, 3, 14, 30),
-        EndTime: new Date(2024, 7, 3, 15, 30),
+        StartTime: new Date(2024, 7, 3, 13),
+        EndTime: new Date(2024, 7, 3, 14, 30),
         IsAllDay: false,
-        ProjectId: 2,
-        TaskId: 2
+        ProjectId: 1,
+        TaskId: 1,
+
     },{
         Id: 2,
         Subject: 'Daily Standup',
@@ -16,30 +17,34 @@ export let timelineResourceData: Object[] = [
         EndTime: new Date(2024, 7, 3, 10, 30),
         IsAllDay: true,
         ProjectId: 2,
-        TaskId: 2
+        TaskId: 2,
+    
     }, {
         Id: 3,
         Subject: 'task day two',
         StartTime: new Date(2024, 7, 4, 13, 30),
         EndTime: new Date(2024, 7, 4, 16, 30),
         IsAllDay: false,
-        ProjectId: 2,
-        TaskId: 1
+        ProjectId: 3,
+        TaskId: 3,
+
     }, {
         Id: 4,
         Subject: 'task day three',
         StartTime: new Date(2024, 7, 4, 9),
         EndTime: new Date(2024, 7, 4, 10, 30),
         IsAllDay: false,
-        ProjectId: 1,
-        TaskId: 1
+        ProjectId: 4,
+        TaskId: 4,
+
     }, {
         Id: 5,
         Subject: 'task day four',
         StartTime: new Date(2024, 7, 4, 12),
         EndTime: new Date(2024, 7, 4, 13),
         IsAllDay: false,
-        ProjectId: 1,
-        TaskId: 1
+        ProjectId: 5,
+        TaskId: 5,
+     
     }
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,31 @@ import {
  ScheduleComponent,
    ViewsDirective, ViewDirective,
    Day, Week, WorkWeek, Month, Agenda,
-    EventSettingsModel, Inject
+    EventSettingsModel, Inject,ResourcesDirective,
 } from '@syncfusion/ej2-react-schedule';
 import { timelineResourceData } from './data/SchaduleData'
 import { registerLicense } from '@syncfusion/ej2-base';
+import './customStyle.css'
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhAYVFpR2Nbe052flZGalxWVAciSV9jS3pTfkVgWXldcHRdT2FfVQ==');
 
 export default function Home() {
 
   const eventSettings: EventSettingsModel = { dataSource: timelineResourceData }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
    <ScheduleComponent
+   cssClass='my-custom-schedule'
     width='100%'
      height='550px'
       currentView='Week' 
       selectedDate={new Date(2024, 7, 3)}
       enableAdaptiveUI={true} 
        eventSettings={eventSettings}>
+
         <ViewsDirective>
           <ViewDirective option='Day' />
-          <ViewDirective option='Week' />
+          <ViewDirective option='Week'/>
           <ViewDirective option='Month' />
         </ViewsDirective> 
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
