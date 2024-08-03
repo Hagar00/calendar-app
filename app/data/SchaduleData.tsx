c3d9f8ca5b3f28bdac70a4119d475167edd427b0
Hagar Hassan <hagar.hassan@nweave.com>
2024-08-03 14:58:31 +0300

    create calendar app

---
+++ app/data/SchaduleData.tsx
@@ -0,0 +1,45 @@
+//this is data of calender as example 
+
+export let timelineResourceData: Object[] = [
+    {
+        Id: 61,
+        Subject: 'Work site',
+        StartTime: new Date(2024, 7, 3, 14, 30),
+        EndTime: new Date(2024, 7, 3, 15, 30),
+        IsAllDay: false,
+        ProjectId: 2,
+        TaskId: 2
+    },{
+        Id: 61,
+        Subject: 'Daily Standup',
+        StartTime: new Date(2024, 8, 4, 9, 30),
+        EndTime: new Date(2024, 8, 4, 10, 30),
+        IsAllDay: true,
+        ProjectId: 2,
+        TaskId: 2
+    }, {
+        Id: 62,
+        Subject: 'task day two',
+        StartTime: new Date(2024, 9, 4, 13, 30),
+        EndTime: new Date(2024, 9, 4, 16, 30),
+        IsAllDay: false,
+        ProjectId: 2,
+        TaskId: 1
+    }, {
+        Id: 63,
+        Subject: 'task day three',
+        StartTime: new Date(2024, 9, 4, 9),
+        EndTime: new Date(2024, 9, 4, 10, 30),
+        IsAllDay: false,
+        ProjectId: 1,
+        TaskId: 1
+    }, {
+        Id: 64,
+        Subject: 'task day four',
+        StartTime: new Date(2024, 10, 4, 12),
+        EndTime: new Date(2024, 10, 4, 13),
+        IsAllDay: false,
+        ProjectId: 1,
+        TaskId: 1
+    }
+];
---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import {
+ ScheduleComponent,
+   ViewsDirective, ViewDirective,
+   Day, Week, WorkWeek, Month, Agenda,
+    EventSettingsModel, Inject
+} from '@syncfusion/ej2-react-schedule';
+import { timelineResourceData } from './data/SchaduleData'
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhAYVFpR2Nbe052flZGalxWVAciSV9jS3pTfkVgWXldcHRdT2FfVQ==');
+
+export default function Home() {
+
+  const eventSettings: EventSettingsModel = { dataSource: timelineResourceData }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   <ScheduleComponent
+    width='100%'
+     height='550px'
+      currentView='Day' 
+      selectedDate={new Date(2024, 7, 3)}
+      enableAdaptiveUI={true} 
+       eventSettings={eventSettings}>
+        {/* <ViewsDirective>
+          <ViewDirective option='Day' />
+          <ViewDirective option='Week' />
+          <ViewDirective option='Month' />
+        </ViewsDirective> */}
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent> 
+    </main>
+  );
+}
